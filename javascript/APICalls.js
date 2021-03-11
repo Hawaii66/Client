93e73ba26a4164864a21618447ec0969141bce7b
Sebastian Ahlman <hawaiilive@outlook.com>
2021-03-11 18:43:53 +0100

    API integration and create pizza tab

--- javascript/APICalls.js
+++ javascript/APICalls.js
@@ -1,8 +1,18 @@
-function TEST(callback) {
-    console.log("TEST");
-    callback();
-}
+const localhost = "http://localhost:5000/"
+const websiteURL = "";
+const MAIN_URL = localhost
+const GetPizzasWithOptions = MAIN_URL + "SearchPizzas";
 
 function SearchForPizzas(searchOptions, callback) {
-
+    console.log(searchOptions);
+    fetch(GetPizzasWithOptions, {
+            method: "POST",
+            body: JSON.stringify(searchOptions),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(res => res.json())
+        .then(k => {
+            console.log(k);
+        });
 }
